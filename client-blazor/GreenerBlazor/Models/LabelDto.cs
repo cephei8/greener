b2b987a220cf74c93e391ac066af6b92c8d6fc57
cephei8 <208798380+cephei8@users.noreply.github.com>
2025-10-26 16:47:37 +0100

    Fix labels display

--- client-blazor/GreenerBlazor/Models/LabelDto.cs
+++ client-blazor/GreenerBlazor/Models/LabelDto.cs
@@ -4,7 +4,5 @@ public class LabelDto
 {
     public required string Key { get; set; }
     public string? Value { get; set; }
-    public required int UserId { get; set; }
-    public required string SessionId { get; set; }
     public required DateTime CreatedAt { get; set; }
 }
