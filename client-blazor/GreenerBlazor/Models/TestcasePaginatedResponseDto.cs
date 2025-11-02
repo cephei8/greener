10c43ed355e51f57edfdfc09b500295c3a971672
cephei8 <208798380+cephei8@users.noreply.github.com>
2025-11-02 17:05:41 +0100

    Fix case with null aggregated status

--- client-blazor/GreenerBlazor/Models/TestcasePaginatedResponseDto.cs
+++ client-blazor/GreenerBlazor/Models/TestcasePaginatedResponseDto.cs
@@ -2,5 +2,5 @@ namespace GreenerBlazor.Models;
 
 public class TestcasePaginatedResponseDto: PaginatedResponseDto<TestcaseDto>
 {
-    public required TestcaseStatus AggregatedStatus { get; set; }
+    public required TestcaseStatus? AggregatedStatus { get; set; }
 }
